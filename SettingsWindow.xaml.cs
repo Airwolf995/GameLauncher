11925142390ae6282f177867cbc1cda85f6536b5
Airwolf995 <airwolfps@gmail.com>
2026-06-28 01:27:26 +0200

    Einstellung Seite Blitz nicht mehr weiß auf

--- SettingsWindow.xaml.cs
+++ SettingsWindow.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Windows;
 using GameLauncher.Models;
+using GameLauncher.Services;
 using GameLauncher.Services.Localization;
 using GameLauncher.ViewModels;
 
@@ -10,17 +11,11 @@ namespace GameLauncher
     {
         private readonly LocalizationService _localization = LocalizationService.Instance;
 
-        [System.Runtime.InteropServices.DllImport("dwmapi.dll", PreserveSig = true)]
-        public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
-
-        private const int DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
-
         protected override void OnSourceInitialized(EventArgs e)
         {
             base.OnSourceInitialized(e);
-            IntPtr hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
-            int darkMode = 1;
-            DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, ref darkMode, sizeof(int));
+            DarkModeHelper.EnableDarkTitleBar(this);
+            Opacity = 1;
         }
 
         public SettingsWindow(GameManager gameManager, Action<string> onThemeChanged, Action<UISettings> onSettingsChanged)
