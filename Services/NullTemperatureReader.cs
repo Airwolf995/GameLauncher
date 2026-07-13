80874b1156561a057a355fbb078ecb8555a4b725
Airwolf995 <airwolfps@gmail.com>
2026-07-13 13:00:44 +0200

    Bereinige ungenutzte Dateien und Logo-Asset

--- Services/NullTemperatureReader.cs
+++
@@ -1,9 +0,0 @@
-namespace GameLauncher.Services
-{
-    internal sealed class NullTemperatureReader : IOptionalTemperatureReader
-    {
-        public float? TryReadCpuTemperature() => null;
-
-        public float? TryReadGpuTemperature() => null;
-    }
-}
