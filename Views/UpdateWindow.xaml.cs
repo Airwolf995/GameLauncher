9f199db7880b5614df0ba128908652dffa83f029
Airwolf995 <airwolfps@gmail.com>
2026-09-02 20:37:14 +0200

    Links in Updatehinweisen anklickbar darstellen

--- Views/UpdateWindow.xaml.cs
+++ Views/UpdateWindow.xaml.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
+using System.Windows.Navigation;
 using GameLauncher.Services;
 using GameLauncher.Services.Localization;
 
@@ -11,6 +13,10 @@ namespace GameLauncher
 {
     public partial class UpdateWindow : Window
     {
+        private static readonly Regex ChangelogMarkdownPattern = new(
+            @"\*\*(?<bold>.+?)\*\*|\[(?<linkText>[^\]]+)\]\((?<url>https?://[^)\s]+)\)",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
         private readonly UpdateService _updateService;
         private readonly UpdateInfo _updateInfo;
         private readonly LocalizationService _localization = LocalizationService.Instance;
@@ -53,23 +59,59 @@ namespace GameLauncher
 
         private static void AddFormattedLine(TextBlock textBlock, string line)
         {
-            MatchCollection boldSegments = Regex.Matches(line, @"\*\*(.+?)\*\*");
+            MatchCollection formattedSegments = ChangelogMarkdownPattern.Matches(line);
             int currentIndex = 0;
 
-            foreach (Match segment in boldSegments)
+            foreach (Match segment in formattedSegments)
             {
                 if (segment.Index > currentIndex)
                 {
                     textBlock.Inlines.Add(new Run(line[currentIndex..segment.Index]));
                 }
 
-                textBlock.Inlines.Add(new Bold(new Run(segment.Groups[1].Value)));
+                if (segment.Groups["bold"].Success)
+                {
+                    textBlock.Inlines.Add(new Bold(new Run(segment.Groups["bold"].Value)));
+                }
+                else if (Uri.TryCreate(segment.Groups["url"].Value, UriKind.Absolute, out Uri? uri) &&
+                         (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps))
+                {
+                    var hyperlink = new Hyperlink(new Run(segment.Groups["linkText"].Value))
+                    {
+                        NavigateUri = uri,
+                        ToolTip = uri.AbsoluteUri
+                    };
+                    hyperlink.SetResourceReference(TextElement.ForegroundProperty, "AccentColor");
+                    hyperlink.RequestNavigate += OpenChangelogLink;
+                    textBlock.Inlines.Add(hyperlink);
+                }
+                else
+                {
+                    textBlock.Inlines.Add(new Run(segment.Value));
+                }
+
                 currentIndex = segment.Index + segment.Length;
             }
 
             if (currentIndex < line.Length)
             {
                 textBlock.Inlines.Add(new Run(line[currentIndex..]));
+            }
+        }
+
+        private static void OpenChangelogLink(object sender, RequestNavigateEventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri)
+                {
+                    UseShellExecute = true
+                });
+                e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                Models.Logger.Error("Link aus den Versionshinweisen konnte nicht geöffnet werden", ex);
             }
         }
 
